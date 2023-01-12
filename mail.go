fc607f685d6ebe35c16ca3be1e04cd331c5bef52
Mark Hendrickson <cmarkh719@gmail.com>
2023-01-12 15:52:39 -0500

    init

---
+++ mail.go
@@ -0,0 +1,44 @@
+package mail
+
+import (
+	"errors"
+	"net"
+	"net/smtp"
+	"strconv"
+	"strings"
+)
+
+type Account struct {
+	Name     string //full name of account user
+	Email    string
+	Password string
+	Host     string //ie smtp.gmail.com
+	Port     int    //ie 587
+}
+
+// NewGmail returns an account struct with a default server and port for Gmail
+func NewGmail(name, email, password string) (a Account) {
+	return Account{name, email, password,
+		"smtp.gmail.com",
+		587,
+	}
+}
+
+func (a Account) Send(subject, body string, to ...string) (err error) {
+	if len(to) == 0 {
+		return errors.New("recipients cannot be blank")
+	}
+
+	msg := "From: " + a.Name + "\r\n" +
+		"To: " + strings.Join(to, ",") + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"Content-Type: text/html\r\n\r\n" + body
+
+	auth := smtp.PlainAuth("", a.Email, a.Password, a.Host)
+	err = smtp.SendMail(net.JoinHostPort(a.Host, strconv.Itoa(a.Port)), auth, a.Email, to, []byte(msg))
+	if err != nil {
+		return
+	}
+
+	return
+}
